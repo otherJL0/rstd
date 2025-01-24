7e0bcd4c01557fb8e327eb8337ed1a637d518277
Jonathan Lopez <jonathanglopez@gmail.com>
2025-01-24 15:50:21 -0500

    Ensure same types of errors are thrown

--- python/tests/test_math.py
+++ python/tests/test_math.py
@@ -1,8 +1,27 @@
-import pytest
-import itertools as it
 import math
 
+import pytest
 import rstd.math as rust_math
+
+
+def test_negative_value_raises_value_error():
+    with pytest.raises(ValueError) as python_error:
+        math.factorial(-9)
+
+    with pytest.raises(ValueError) as rust_error:
+        rust_math.factorial(-9)  # type: ignore
+
+    assert type(python_error.value) == type(rust_error.value)
+
+
+def test_float_value_produces_type_error():
+    with pytest.raises(TypeError) as python_error:
+        math.factorial(8.9)  # type: ignore
+
+    with pytest.raises(TypeError) as rust_error:
+        rust_math.factorial(8.9)
+
+    assert type(python_error.value) == type(rust_error.value)
 
 
 def test_factorial():
